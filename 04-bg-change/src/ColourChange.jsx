6f4ea668f15644b8e1631212cd7786a92d6c9320
yogesh7122002 <yogeshdeshmane21@gmail.com>
2024-08-14 15:39:42 +0530

    adding local Files

---
+++ 04-bg-change/src/ColourChange.jsx
@@ -0,0 +1,30 @@
+function ColourChange() {
+    return (
+        <>
+            <h2>Hello World</h2>
+        </>
+    )
+}
+export default colourChange
+
+
+
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import "./colourChange"
+
+// function App() {
+//     // const [count, setCount] = useState(0)
+//     const [colour, setColour] = useState("olive")
+
+//     return (
+//         <>
+//             <colourChange />
+//         </>
+//     )
+// }
+
+// export default App
---
+++ Java Script Refresh/veriables.js
@@ -0,0 +1,16 @@
+// const accountId = 12;
+// let accountEmail= "yogeshdeshmane21@gmail.com";
+// var accountPass ="123454";
+// accountCity ="benglore";
+"use strict"
+let account = "Instagarm";
+console.log(account)
+
+// console.table([accountId,accountEmail,accountPass,accountCity]);
+
+// // accountId=13;
+// accountEmail= "yogesh@gmail.com";
+// accountPass ="1234342";
+// accountCity ="Kolahapur";
+// console.table([accountId,accountEmail,accountPass,accountCity]);
+
