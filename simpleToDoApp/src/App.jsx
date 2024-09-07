686c5c2ae703a570e8175b8185b95707e2af1b08
yogesh7122002 <yogeshdeshmane21@gmail.com>
2024-09-07 13:44:28 +0530

    Tailwind is not working

---
+++ simpleToDoApp/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='text-3xl'> The Name Is Yogesh</h2>
+    </>
+  )
+}
+
+export default App
